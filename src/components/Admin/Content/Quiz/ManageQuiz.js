ea6ed1a4b46fec1c7a6c292b50395107411a681b
Nghia Pham Trong <nghiaphamjb@gmail.com>
2024-04-20 11:32:16 +0700

    design update/feature

--- src/components/Admin/Content/Quiz/ManageQuiz.js
+++ src/components/Admin/Content/Quiz/ManageQuiz.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
 import "./ManageQuiz.scss"
+import { useState } from "react";
 import Select from 'react-select'
 import {postCreateNewquiz} from "../../../../services/apiServices"
 import { toast } from 'react-toastify';
 import TableQuiz from "../Quiz/TableQuiz"
 import Accordion from 'react-bootstrap/Accordion';
+import QuizQA from "./QuizQA";
+import AssignQuiz from "./AssignQuiz";
 
 const options = [
     { value: 'EASY', label: 'EASY' },
@@ -92,13 +94,29 @@ const ManageQuiz = (props)=> {
                             </div>
                             </fieldset>
                         </div>
+                        <div className="list-detail">
+                            <TableQuiz/>
+                        </div>
                     </Accordion.Body>
                 </Accordion.Item>
+
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Update Q/A Quizzes</Accordion.Header>
+                    <Accordion.Body>
+                        <QuizQA/>
+                    </Accordion.Body>
+                </Accordion.Item>
+
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>Assign to Users</Accordion.Header>
+                    <Accordion.Body>
+                        <AssignQuiz/>
+                    </Accordion.Body>
+                </Accordion.Item>
+
             </Accordion>
           
-           <div className="list-detail">
-                <TableQuiz/>
-           </div>
+          
         </div>
     )
 }
